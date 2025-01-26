f42b49ed5c927ded90ba72a70ba656a8ae007e0a
Deep <193667101+DeepSideEffect@users.noreply.github.com>
2025-01-27 00:51:43 +0100

    Ajout des sous répertoires code-analysis et .scannerwork au gitignore + Ajustement sécurité suite au retour sonar + rectifications en cas de liens relatifs

--- index.mjs
+++ index.mjs
@@ -1,11 +1,18 @@
 import express from 'express';
 import fetch from 'node-fetch';
 import morgan from 'morgan';
+import helmet from 'helmet';
+import { URL } from 'url';
 
+const entryEndpoint = '/proxy';
 const app = express();
 const port = process.env.PORT || 3000; // Ex. to change the port : >npx cross-env PORT=3001 node index.mjs< ou >npx cross-env PORT=3001 npm run start<
 let targetUrlBase = '';
+let targetBasePath = '';
 
+//#region Middlewares
+
+app.use(helmet());
 app.use(morgan('dev'));
 
 app.use('*', (req, res, next) => {
@@ -17,15 +24,24 @@ app.use('*', (req, res, next) => {
 	next();
 });
 
-app.get('/proxy', async (req, res) => {
-	const targetUrl = req.query?.url;
+//#endregion Middlewares
+
+//#region Routes
+
+app.get(entryEndpoint, async (req, res) => {
+	let targetUrl = req.query?.url;
 	if (!targetUrl) {
-		return res.status(400).send('URL manquante');
+		if (!targetUrlBase || req.url.includes('http'))
+			return res.status(400).send('URL manquante');
+		else {
+			const cleanUrl = req.url.replace(entryEndpoint, '');
+			targetUrl = targetUrlBase.concat(targetBasePath).concat(cleanUrl);
+			return getAndSendData(res, targetUrl);
+		}
 	}
 
-	const decodedUrl = decodeURIComponent(targetUrl);
-	targetUrlBase = decodedUrl;
-	getAndSendData(res, decodedUrl);
+	targetUrlBase = getTargetUrlBase(targetUrl);
+	getAndSendData(res, targetUrl);
 });
 
 app.get('/*', async (req, res) => {
@@ -41,12 +57,17 @@ app.listen(port, () => {
 	}
 });
 
+//#endregion Routes
+
+//#region Helpers
+
 async function getAndSendData(res, targetUrl) {
 	console.log(`*** target url: ${targetUrl}`);
 	console.log('*****************************');
 
 	try {
-		const response = await fetch(targetUrl);
+		const decodedUrl = decodeURIComponent(targetUrl);
+		const response = await fetch(decodedUrl);
 		const contentType = response.headers.get('content-type');
 		res.set('Access-Control-Allow-Origin', '*');
 		res.set('Content-Type', contentType);
@@ -58,3 +79,16 @@ async function getAndSendData(res, targetUrl) {
 		res.status(500).send('Erreur lors de la récupération des données');
 	}
 }
+
+function getTargetUrlBase(targetUrl) {
+	const parsedUrl = new URL(targetUrl);
+	targetBasePath = parsedUrl.pathname;
+	const protocol = parsedUrl.protocol;
+	const domain = parsedUrl.hostname;
+	const rootUrl = `${protocol}//${domain}`;
+	console.log(`*** Requested root url: ${rootUrl} ***`);
+
+	return decodeURIComponent(rootUrl);
+}
+
+//#endregion Helpers
